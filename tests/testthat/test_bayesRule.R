84527ca582b1f0137efbd7d3293659350ad0bda0
Constantino Antonio García Martínez <constantinoantonio.garcia@usc.es>
2016-08-03 17:30:46 +0200

    Add test for the bayes rule function

---
+++ tests/testthat/test_bayesRule.R
@@ -0,0 +1,16 @@
+skip_on_cran()
+
+library('fracdet', quietly = TRUE)
+
+test_that("bayes rule works as expected", {
+  data = readRDS("../testdata/bayesRule.RDS")
+  # tolerance has to be quite high due to the difficulties in simulating
+  # the highest Hs
+  result = apply(data,MARGIN = 1, function(x){
+    bayesRuleCpp(x[["d"]], as.list(x[2:7]))
+  })
+  expect_equal(result, data$output, tolerance = 1e-5)
+})
+
+
+
